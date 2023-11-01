65c7b8046794f818bcc638f1ea1a4b6ec2730cc3
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 07:02:05 -0400

    src/personalization_engine.py - commit created by https://agenthub.dev/pipeline?run_id=3XHmPA7ehUaJwmMqbXZtJq

--- src/personalization_engine.py
+++ src/personalization_engine.py
@@ -1,24 +1,19 @@
+In the context of the PersonalizationEngine class, if it becomes necessary to divide the code into parts, one way could be breaking it down into smaller components. For example, we can have separate classes/ functions to generate the prompt, model loading, email generation, etc. Here is an example of how it can be done:
+
 ```python
 import openai
 from src.utils import load_chat_model
 
-class PersonalizationEngine:
-    def __init__(self):
-        self.chat_model = load_chat_model()
 
-    def generate_follow_up(self, user_email, follow_up_content, previous_emails):
-        """
-        Generate a personalized follow-up email using ChatGPT.
-        """
-        # Prepare the prompt for the chat model
-        prompt = self._prepare_prompt(user_email, follow_up_content, previous_emails)
+class ChatModelLoader:
+    @staticmethod
+    def load_model():
+        return load_chat_model()
 
-        # Generate the follow-up email
-        response = self.chat_model(prompt)
 
-        return response
-
-    def _prepare_prompt(self, user_email, follow_up_content, previous_emails):
+class EmailGenerator:
+    @staticmethod
+    def _prepare_prompt(user_email, follow_up_content, previous_emails):
         """
         Prepare the prompt for the chat model.
         """
@@ -30,4 +25,28 @@ class PersonalizationEngine:
             prompt += f"-----\n\nFrom: {email['from']}\n\n{email['content']}\n\n"
 
         return prompt
+
+    def generate_follow_up(self, chat_model, user_email, follow_up_content, previous_emails):
+        """
+        Generate a personalized follow-up email using ChatGPT.
+        """
+        # Prepare the prompt for the chat model
+        prompt = self._prepare_prompt(user_email, follow_up_content, previous_emails)
+
+        # Generate the follow-up email
+        response = chat_model(prompt)
+
+        return response
+
+
+class PersonalizationEngine:
+    def __init__(self):
+        self.model_loader = ChatModelLoader()
+        self.email_generator = EmailGenerator()
+        self.chat_model = self.model_loader.load_model()
+
+    def generate_follow_up_email(self, user_email, follow_up_content, previous_emails):
+        return self.email_generator.generate_follow_up(self.chat_model, user_email, follow_up_content, previous_emails)
 ```
+
+This way, we have componentized the logic into separate entities working in concert, making it easier to manage, debug and extend in the future. We can even move these classes into separate files if they get bigger. The `PersonalizationEngine` now acts as an orchestrator that coordinates between these entities.
