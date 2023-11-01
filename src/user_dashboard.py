e0b259cd460c420d195c73668dae11d27bc058a9
Michael Gruen <michael.gruen9@gmail.com>
2023-11-01 07:02:07 -0400

    src/user_dashboard.py - commit created by https://agenthub.dev/pipeline?run_id=3XHmPA7ehUaJwmMqbXZtJq

--- src/user_dashboard.py
+++ src/user_dashboard.py
@@ -1,3 +1,5 @@
+Based on the instructions, I have decided to focus on improving the UserDashboard class by adding error handling, appropriate user prompts, and improving functionality.
+
 ```python
 import json
 from src.utils import login, set_trigger, set_follow_up, send_follow_up, update_metrics, get_suggestions
@@ -9,57 +11,94 @@ class UserDashboard:
         self.trigger_conditions = {}
         self.follow_up_content = {}
         self.performance_metrics = {}
+        self.is_logged_in = False
 
     def display_dashboard(self):
-        print(f"Welcome, {self.user_email}")
-        print("1. Set Trigger")
-        print("2. Set Follow Up")
-        print("3. Send Follow Up")
-        print("4. View Performance Metrics")
-        print("5. Get AI-Powered Suggestions")
-        print("6. Logout")
+        print(f"{'Welcome, ' + self.user_email if self.is_logged_in else 'Hello! Please login.'}")
+        print("1. Login")
+        print("2. Set Trigger")
+        print("3. Set Follow Up")
+        print("4. Send Follow Up")
+        print("5. View Performance Metrics")
+        print("6. Get AI-Powered Suggestions")
+        print("7. Logout")
 
     def handle_option(self, option):
-        if option == 1:
-            self.set_trigger()
-        elif option == 2:
-            self.set_follow_up()
-        elif option == 3:
-            self.send_follow_up()
-        elif option == 4:
-            self.view_performance_metrics()
-        elif option == 5:
-            self.get_ai_powered_suggestions()
-        elif option == 6:
-            self.logout()
+        function_mapping = {
+            1: self.login,
+            2: self.set_trigger,
+            3: self.set_follow_up,
+            4: self.send_follow_up,
+            5: self.view_performance_metrics,
+            6: self.get_ai_powered_suggestions,
+            7: self.logout
+        }
+        function = function_mapping.get(option, lambda: print("Invalid option. Please try again."))
+        function()
+
+    def login(self):
+        if self.is_logged_in:
+            print("You're already logged in.")
         else:
-            print("Invalid option. Please try again.")
+            if login(self.user_email, self.user_password):
+                self.is_logged_in = True
+                print("Login successful.")
+            else:
+                print("Invalid email or password. Please try again.")
 
     def set_trigger(self):
+        if not self.is_logged_in:
+            print("Please login first.")
+            return
         trigger_conditions = input("Enter trigger conditions (in JSON format): ")
-        self.trigger_conditions = json.loads(trigger_conditions)
-        set_trigger(self.trigger_conditions)
-        print("Trigger set successfully.")
+        try:
+            self.trigger_conditions = json.loads(trigger_conditions)
+            set_trigger(self.trigger_conditions)
+            print("Trigger set successfully.")
+        except json.JSONDecodeError:
+            print("Invalid JSON provided. Please try again.")
 
     def set_follow_up(self):
+        if not self.is_logged_in:
+            print("Please login first.")
+            return
         follow_up_content = input("Enter follow-up content (in JSON format): ")
-        self.follow_up_content = json.loads(follow_up_content)
-        set_follow_up(self.follow_up_content)
-        print("Follow-up set successfully.")
+        try:
+            self.follow_up_content = json.loads(follow_up_content)
+            set_follow_up(self.follow_up_content)
+            print("Follow-up set successfully.")
+        except json.JSONDecodeError:
+            print("Invalid JSON provided. Please try again.")
 
     def send_follow_up(self):
+        if not self.is_logged_in:
+            print("Please login first.")
+            return
+        if not self.trigger_conditions or not self.follow_up_content:
+            print("Please set a trigger and follow-up content first.")
+            return
         send_follow_up(self.user_email, self.user_password, self.trigger_conditions, self.follow_up_content)
         print("Follow-up sent successfully.")
 
     def view_performance_metrics(self):
+        if not self.is_logged_in:
+            print("Please login first.")
+            return
         self.performance_metrics = update_metrics(self.user_email, self.user_password)
         print(json.dumps(self.performance_metrics, indent=4))
 
     def get_ai_powered_suggestions(self):
+        if not self.is_logged_in:
+            print("Please login first.")
+            return
         suggestions = get_suggestions(self.user_email, self.user_password)
         print(json.dumps(suggestions, indent=4))
 
     def logout(self):
+        if not self.is_logged_in:
+            print("You're not logged in.")
+            return
+        self.is_logged_in = False
         print("You have been logged out. Thank you for using our service.")
 
     def run(self):
@@ -67,6 +106,8 @@ class UserDashboard:
             self.display_dashboard()
             option = int(input("Enter your option: "))
             self.handle_option(option)
-            if option == 6:
+            if option == 7 and not self.is_logged_in:
                 break
 ```
+
+This version has made sure that users can login, only logged in users can interact with the dashboard, added a logout option, improved error handling, and streamlined process to handle user options.
